32c8694b818c205ea9e15994a34cc53b5c08712d
drachen2299 <benlowry55@gmail.com>
2021-11-03 10:34:20 -0600

    added grid stuff

--- client/pages/comparison/index.js
+++ client/pages/comparison/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export default function Comparisons () {
+export default function Comparisons() {
   return (
-    <div>
-      <div className="inline-block">
+    <div className="grid grid-cols-2 grid-rows-5 gap-4 mx-auto my-0 max-w-3xl pt-4">
+      <div className="inline-block text-center border-box border">
         <h3>Common Languages</h3>
-        <ul>
+        <ul className="p-4">
           <li>Python</li>
           <li>Java</li>
           <li>Golang</li>
@@ -13,9 +13,9 @@ export default function Comparisons () {
           <li>ASP.NET</li>
         </ul>
       </div>
-      <div className="inline-block">
+      <div className="inline-block text-center border-box border">
         <h3>Common Languages</h3>
-        <ul>
+        <ul className="pt-4">
           <li>Python</li>
           <li>Java</li>
           <li>Golang</li>
@@ -23,7 +23,13 @@ export default function Comparisons () {
           <li>ASP.NET</li>
         </ul>
       </div>
+      <form className="col-span-2 grid grid-rows-2 grid-cols-4" action="">
+        <input className="row-start-1 row-end-2 col-end-2" type="text" />
+        <button className="row-start-1 row-end-2 col-end-3">search</button>
+        <input className="col-2 col-end-4" type="text" />
+        <button className="col-span-1 col-end-5">search</button>
+        <button>submit</button>
+      </form>
     </div>
   );
-};
-
+}
