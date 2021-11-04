cb746612bd94727a1e7773bdc71606d7a321ace3
drachen2299 <benlowry55@gmail.com>
2021-11-04 12:31:34 -0600

    -added select placeholder options

--- client/pages/survey/index.js
+++ client/pages/survey/index.js
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 
 export default function UserSurvey() {
-  
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [ageRange, setAgeRange] = useState("");
@@ -21,7 +20,7 @@ export default function UserSurvey() {
       type_of_dev: typeOfDev,
     };
     await createSurvey(surveyAnswers);
-    router.push('/user-results');
+    router.push("/user-results");
   };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
@@ -32,9 +31,19 @@ export default function UserSurvey() {
           onSubmit={handleSubmit}
         >
           <label htmlFor="username">Username:</label>
-          <input className="inline-block" value={username} type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)}/>
+          <input
+            className="inline-block"
+            value={username}
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <h3>language:</h3>
-          <select value={languageId} onChange={(e) => setLanguageId(e.target.value)}>
+          <select
+            value={languageId}
+            onChange={(e) => setLanguageId(e.target.value)}
+          >
+            <option value="" disabled selected>Choose a Language</option>
             <option value="1">Python</option>
             <option value="2">Ruby</option>
             <option value="3">Java</option>
@@ -43,12 +52,20 @@ export default function UserSurvey() {
             <option value="6">Rust</option>
           </select>
           <h3>Age-Range:</h3>
-          <select value={ageRange} onChange={(e) => setAgeRange(e.target.value)}>
+          <select
+            value={ageRange}
+            onChange={(e) => setAgeRange(e.target.value)}
+          >
+            <option value="" disabled selected>Choose an Age Range</option>
             <option value="20-40">20-40</option>
             <option value="40-60">40-60</option>
           </select>
           <h3>Industry:</h3>
-          <select value={industry} onChange={(e) => setIndustry(e.target.value)}>
+          <select
+            value={industry}
+            onChange={(e) => setIndustry(e.target.value)}
+          >
+            <option value="" disabled selected>Choose an Industry</option>
             <option value="Information Technology">
               Information Technology
             </option>
@@ -60,7 +77,11 @@ export default function UserSurvey() {
             <option value="other">Other</option>
           </select>
           <h3>Employment Status:</h3>
-          <select value={typeOfDev} onChange={(e) => setTypeOfDev(e.target.value)}>
+          <select
+            value={typeOfDev}
+            onChange={(e) => setTypeOfDev(e.target.value)}
+          >
+            <option value="" disabled selected>Choose your Dev Type</option>
             <option value="enthusiast">Enthusiast</option>
             <option value="professional">Professional</option>
           </select>
