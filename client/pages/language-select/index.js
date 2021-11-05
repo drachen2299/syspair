c5f6f6b4c8dcc56f8ec9cda91a8499e5a88cd005
drachen2299 <benlowry55@gmail.com>
2021-11-04 21:36:10 -0600

    -init language selector

---
+++ client/pages/language-select/index.js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+export default function LanguageSelect() {
+  const router = useRouter()
+  
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+LanguageSelect;
