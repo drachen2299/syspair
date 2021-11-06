c8368b82865a97ecb95ce4e80cf0179076ded315
drachen2299 <benlowry55@gmail.com>
2021-11-06 14:48:22 -0600

    -re-created nav with next BP

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,12 +1,30 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Nav() {
+  const username = null;
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="#">About</Link>
+      <ul>
+        <li>
+          <Link href="/">
+            <button>Home</button>
+          </Link>
+        </li>
+        {username && (
+          <>
+            <li></li>
+            <li></li>
+          </>
+        )}
+        {!username && (
+        <li>
+          <Link href="/survey">
+            <button>Survey</button>
+          </Link>
+        </li>)}
+      </ul>
     </nav>
   );
-};
+}
 
- Nav;
+Nav;
