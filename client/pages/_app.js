45c0791de36c058388abb1b727f42e3b9ed1982c
drachen2299 <benlowry55@gmail.com>
2021-11-07 14:25:46 -0700

    deleted react app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,19 +5,12 @@ import { provideAuth } from "../lib/use-auth";
 
 function MyApp({ Component, pageProps }) {
   return (
-<<<<<<< HEAD
     <provideAuth>
       <userContext.Provider value={{ usersName: "ben" }}>
         <Nav />
         <Component {...pageProps} />
       </userContext.Provider>
     </provideAuth>
-=======
-    <userContext.Provider>
-      <Nav />
-      <Component {...pageProps} />
-    </userContext.Provider>
->>>>>>> old-state
   );
 }
 
