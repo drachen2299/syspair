f387351bbfbec48cbe151648cbdf106d445cb36b
drachen2299 <benlowry55@gmail.com>
2021-11-08 10:19:47 -0700

    last minute style

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -4,7 +4,7 @@ export default function Nav() {
   const username = null;
   return (
     <nav>
-      <ul className=" box-border p-1 inline-block flex justify-between w-[100%]">
+      <ul className=" box-border p-1 flex justify-between w-[100%]">
         <li>
           <Link href="/">
             <button>Home</button>
