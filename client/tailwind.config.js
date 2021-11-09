d130be3ab3e2abb21c26bbd4199c2643aa020c92
drachen2299 <benlowry55@gmail.com>
2021-11-08 23:54:02 -0700

    re-enable jit in tailwindconfig

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,5 @@
-
- module.exports = {
-   mode: 'Jit',
+module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
    darkMode: 'class',
    theme: {
